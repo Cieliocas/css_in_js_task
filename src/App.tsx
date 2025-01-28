b525d6620a9d30b8ae54c93a32550a1b73624db0
cieliocas <francielioevangelista@hotmail.com>
2025-01-28 13:08:58 -0300

    Update styled-components and related types in package.json and package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
+import styled from 'styled-components'
 
 import './global.css'
 
